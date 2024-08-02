e91793f255a0cce958942784e7102cf84154fcf3
Ritik <kumarsinghritik22@gmail.com>
2024-08-02 23:15:07 +0530

    made fixes

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -5,7 +5,6 @@ import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 import Profile from '@components/Profile';
 import React from 'react'
-import { set } from 'mongoose';
 
 const MyProfile = () => {
     const { data: session } = useSession();
